fdfe8b32f45d306ea7653fbf795a04bd8518b238
Vanshika <vanshikag0111@gmail.com>
2021-06-14 22:58:36 +0530

    notes for control structures- flow of execution

---
+++ Basic/flow of execution.cpp
@@ -0,0 +1,11 @@
+//control structures of cpp
+
+/* structure refers to the flow of execution of program.
+   There are basically three types of control structures:
+   1. sequence structure: executes line by line
+   2. selection structure: checks condition for true or false
+      & gives final output
+   3. loop structure: checks for condition gives output then
+      again goes for loop check until the condition stops 
+      satisfying.
+*/
