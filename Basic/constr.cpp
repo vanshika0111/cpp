e5c2515b2d59c3efc6fa4dc9164e180f2697e132
Vanshika <vanshikag0111@gmail.com>
2021-06-19 08:33:17 +0530

    constructor concept

---
+++ Basic/constr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+class A{
+    public:
+    int age;
+    string name;
+    A(int x, string y)
+    {
+        age = x;
+        name =y;
+    }
+};
+
+int main()
+{
+    int age;
+    string name;
+    A obj(3,"das"), obj2(6,"Sevak");
+    cout<<"Age is : " <<obj.age<<endl;
+    cout<<"Name is : " <<obj.name<<endl;
+    cout<<"Age is : " <<obj2.age<<endl;
+    cout<<"Name is : " <<obj2.name<<endl;
+    return 0;
+}
