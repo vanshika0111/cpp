e74c09b9ce4a0df6adaf351420de9eaac716c79e
Vanshika <vanshikag0111@gmail.com>
2021-06-25 22:50:29 +0530

    constructor program

---
+++ Basic/constr2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+class A
+{
+    public :
+    int a=10;
+    A()
+    {
+        a=369;
+    }
+    A(int a)
+    {
+        cout<< "Value of a is : " << a << endl;
+    }
+    void show()
+    {
+        cout<< "Value of a is : " << a << endl;
+    }
+};
+
+int main()
+{
+    //A obj(23);  // gives 23 as output
+    A obj;       // gives 369 as output
+    obj.show();  // gives 10 as output
+    //A obj();  // throws an error
+    //A obj;    // throws an error
+    return 0;
+}
