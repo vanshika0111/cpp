aca8049c5c4acef225707d971affa2b2278e0156
Vanshika <vanshikag0111@gmail.com>
2021-06-27 16:56:09 +0530

    fusion completed

--- Output/fusion.cpp
+++ Output/fusion.cpp
@@ -7,8 +7,10 @@
 using namespace std;
 
 // global declarations
-int balance, bid_amount;
-string PlayerName;
+int balance, bid_amount;         // balance_bid
+int loss;                        // step_loss
+int randomNumber, guess;         // Random_Guess
+string PlayerName;               // intro function
 
 // function prototype
 void rules();
@@ -17,6 +19,8 @@ void balance_bid();
 void GameTemplate();
 void GameLogic();
 void step_loss();
+void Random_Guess();
+void thanks();
 
 // main function
 int main()
@@ -89,6 +93,7 @@ void balance_bid()
 
         
     } while ( (bid_amount > balance) || (bid_amount % 10 != 0) );
+
     GameTemplate();
         // if (bid_amount == balance)
         // {
@@ -109,7 +114,7 @@ void balance_bid()
 }
 
 void GameTemplate()
-// function to 
+// function to display the current balance and bidding amount
 {
     cout<< "Hello " << PlayerName << "!" << endl;
     cout<< "Your balance is Rs. " << balance << endl;
@@ -117,18 +122,86 @@ void GameTemplate()
     balance -= bid_amount;
     cout<< "Now your balance is Rs. " << balance << endl;
     cout<< "Now let's begin." << endl;
+    cout<< "You will get a maximum of 10 chances to guess the correct number." << endl;
+
+    Random_Guess();
+}
+
+void Random_Guess()
+// function to generate a random number &
+// ask user for first guess
+{
+    step_loss();
+
+    srand(time(0));   //  random number: [1,100]
+    randomNumber = (rand() % 100) + 1;
+    //cout<< "Random number generated: " << randomNumber << endl;
+
+    cout<< "Alright! Guess your first number: " ;
+    cin>> guess;
+    cout<< endl;
+
     GameLogic();
 }
 
-void GameLogic()
+void step_loss()
+// function to declare the loss per wrong guesss
 {
-    cout << "done" << endl;
+    loss = bid_amount / 10;
+    cout<< "***ALERT!***" << endl;
+    cout<< "You will lose Rs. "<< loss << " per wrong guess." <<  endl;
 }
 
-void step_loss()
+void GameLogic()
+// function containing the logic of the game
 {
-    
-    cout<< "Ooppss! You just lost a penny from your bid amount. " << endl;
-    cout<< "Now you have Rs. " << bid_amount << endl;
+    do
+    {
+        if ( guess < randomNumber)
+        {
+            bid_amount -= loss;
+            cout<< "Oh no! Wrong guess." << endl;
+            cout<< "Now you have Rs. " << bid_amount << " left with you." << endl;
+            cout<< "Please guess a higher number." << endl;
+            cout<< "Guess again: ";
+            cin>> guess;
+            cout<< endl;
+        }
+
+        else if ( guess > randomNumber)
+        {
+            bid_amount -= loss;
+            cout<< "Oh no! Wrong guess." << endl;
+            cout<< "Now you have Rs. " << bid_amount << " left with you." << endl;
+            cout<< "Please guess a lower number." << endl;
+            cout<< "Guess again: ";
+            cin>> guess;
+            cout<< endl;
+        }
+
+        else if (bid_amount <= 1)
+        {
+            cout<< "Oh dang! You lost all of your bet." << endl;
+            balance -= bid_amount;
+            cout<< "Your current balance is Rs. " << balance << "." << endl;
+            break;
+        }
+    } while ( guess != randomNumber);
+
+    if (guess == randomNumber)
+    {
+        cout<< "Wohooo!! You won the game." << endl;
+        balance += bid_amount*20;
+        cout<< "You have a balance of Rs. " << balance << "." << endl;
+    }
+
+    thanks();
+}
+
+void thanks()
+// function to end the game with a thank you note
+{
+    cout<< "Thank you " << PlayerName << " for playing with us." << endl;
+    cout<< "See you soon :)" << endl;
 }
 
