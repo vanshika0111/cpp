8a455424ed45aa87e579bfc6695f11509fc783c9
Vanshika <vanshikag0111@gmail.com>
2021-07-02 16:47:52 +0530

    oop notes/definitions

---
+++ OOP/concepts of oop.cpp
@@ -0,0 +1,15 @@
+// notes of oop
+
+// 1. CLASS: basic template for creating objects
+
+// 2. DATA ABSTRACTION: representing essential featutres without including the backgroud details 
+
+// 3. ENCAPSULATION: wrapping up data and functions into single unit
+
+// 4. INHERITANCE: inheriting/using the properties of one class into another
+
+// 5. POLYMORPHISM: ability to take more than one form
+
+// 6. DYNAMIC BINDING: code which will execute is not known until the program runs
+
+// 7. MESSAGE PASSING: object message (information) call format
