b89c34ebed35a7bedce5b84f2fa7bf5890ac5a1c
Vanshika <vanshikag0111@gmail.com>
2021-07-05 20:08:00 +0530

    Area of circle, sqaure, rectangle

---
+++ Basic/area.cpp
@@ -0,0 +1,40 @@
+// #include<iostream>
+// #include<stdlib.h>
+// using namespace std;
+
+// int main()
+// {
+//     float radius = 7.0;
+//     float area;
+//     area = 3.14 * radius * radius;
+//     cout<< "Area of circle is " << area << endl;
+
+//     return 0;
+// }
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    int side=6;
+    int area;
+    area = side*side;
+    cout << "Area of sqaure is " << area << endl;
+
+    return 0;
+}
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    int length, breadth, area;
+    area = length * breadth;
+    cout<< "Area of rectangle is " << area << endl;
+
+    return 0;
+}
