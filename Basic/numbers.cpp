29e81b4f7dee7f9119a2f232ea1d069ef538ad4f
Vanshika <vanshikag0111@gmail.com>
2021-07-06 08:00:53 +0530

    print 1 to 10 via for loop

---
+++ Basic/numbers.cpp
@@ -0,0 +1,16 @@
+// program to print numbers from 1 to 10
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=1; i<=10; i++)
+    {
+        cout << "\n" << i;
+    }
+
+    return 0;
+}
