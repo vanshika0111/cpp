98d5688c916ee97223d291661d29df9c17be8c32
Vanshika <vanshikag0111@gmail.com>
2021-07-14 12:27:29 +0530

    student gradecard

---
+++ OOP/Programs/stud.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+const int Obj=3;
+const int size=3;
+
+class Student
+{
+    int roll_number;
+    char name[21];
+    float marks[size];
+
+    public:
+    void get_value(void)
+    {
+        char ch;
+        cout << "Enter details:" << endl;
+        cout << "Roll number: " ;
+        cin >> roll_number;
+        cin.get(ch);
+        // cout << endl;
+        cout << "\n" << "Name: " ;
+        cin.getline(name,21);
+        // cout << endl;
+        cout << "\n" << "Total marks: ";
+        cin >> marks[size];
+        cout << endl;
+    }
+    void display(void);
+};
+
+void Student :: display(void)
+{
+    cout << endl;
+    cout << "Roll number: " << roll_number << endl;
+    cout << "Name: " << name << endl;
+    cout << "Total marks: " << marks << endl;
+}
+
+Student fy[Obj];
+
+int main()
+{
+    // int i=0;
+    // dispalying infromation for three students
+    for(int i=0; i<Obj; i++)
+    {
+        cout << "Student " << (i+1) << endl;
+        fy[i].get_value();
+    }
+
+    for(int i=0; i<Obj; i++)
+    {
+        fy[i].display();
+    }
+
+    return 0;
+}
