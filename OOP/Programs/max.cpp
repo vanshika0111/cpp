9ba4384f3dc4241130db3ca5a1c21e69b2ffcc3d
Vanshika <vanshikag0111@gmail.com>
2021-07-14 18:10:42 +0530

    maximum of three numbers

---
+++ OOP/Programs/max.cpp
@@ -0,0 +1,62 @@
+// program to find greates between 3 numbers by defining
+// function inside the class
+
+#include<iostream>
+#include<stdlib.h> 
+using namespace std;
+
+class Greatest
+{
+    int x,y,z;
+
+    public:
+    // void final(int x, int y, int z)
+    // {
+    //     if(x>y)
+    //     {
+    //         (x>z) ? x : z;
+    //     }
+    //     else
+    //     {
+    //         (y>z) ? y : z;
+    //     }
+
+    // }
+    void final(int x, int y, int z)
+    {
+        if (x>y)
+        {
+            if(x>z)
+            {
+                cout << "The greatest number is " << x << endl;
+
+            }
+            else
+            {
+                cout << "The greatest number is " << z << endl;
+            }   
+        }
+        else
+        {
+            if (y>z)
+            {
+                cout << "The greatest number is " << y << endl;
+            }
+            else
+            {
+                cout << "The greatest number is " << z << endl;
+            }
+                    
+        }
+    }
+
+
+};
+
+int main()
+{
+    Greatest obj1;
+    obj1.final(3,6,2);
+
+    return 0;
+}
