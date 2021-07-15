5ad30469c51f6da363dac9ffb24bd4c70b9e2e79
Vanshika <vanshikag0111@gmail.com>
2021-07-15 11:12:04 +0530

    access specifier using inheritance

---
+++ OOP/Concepts/five.cpp
@@ -0,0 +1,84 @@
+// program to show access to public, private and protected using inheritance
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+// base class
+class Employee
+{
+    
+    public:
+    int id; 
+    float salary;
+    Employee(int inpid)
+    {
+        id = inpid;
+        salary = 34.0;
+    } 
+    Employee(){}
+};
+
+// // derived class: private
+// class Programmer : private Employee
+// {
+//     public:
+//     Programmer(int inpid)
+//     {
+//         id = inpid;
+//     }
+//     int languageCode = 9;
+//     void getData()
+//     {
+//         cout << id << endl;
+//     }
+  
+// };
+
+// derived class: public
+class Programmer : public Employee
+{
+    public:
+    int languageCode;
+    Programmer(int inpid)
+    {
+        id = inpid;
+        languageCode = 9;
+    }
+        
+    void getData()
+    {
+        cout << id << endl;
+    }
+  
+};
+
+
+
+// // main function for private derived class
+// int main()
+// {
+//     Employee abc(1), xyz(2);
+//     cout << abc.salary << endl;
+//     cout << xyz.salary << endl;
+//     Programmer skillF(1);
+//     cout << skillF.languageCode << endl;
+//     skillF.getData();
+//     // cout << skillF.id << endl;
+//     // the above line will give error as it is private
+//     return 0;
+// }
+
+// main function for public derived class
+int main()
+{
+    Employee abc(1), xyz(2);
+    cout << abc.salary << endl;
+    cout << xyz.salary << endl;
+    Programmer skillF(1);
+    cout << skillF.languageCode << endl;
+    skillF.getData();
+    cout << skillF.id << endl;
+    
+    return 0;
+}
