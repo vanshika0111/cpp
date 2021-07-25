929a4741081e3959d3fcf49b91d759b1b0b8dac5
Vanshika <vanshikag0111@gmail.com>
2021-07-25 13:06:44 +0530

    memory allocation using arrays

---
+++ OOP/Concepts/allocate.cpp
@@ -0,0 +1,50 @@
+// objects memory allocations using arrays
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Shop
+{
+private:
+    int ItemID[100];
+    int ItemPrice[100];
+    int counter;
+
+public:
+    void initCounter(void) { counter = 0; }
+    void setPrice(void);
+    void displayPrice(void);
+};
+
+void Shop ::setPrice(void)
+{
+    cout << "Enter ID of your item number " << counter + 1 << ":";
+    cin >> ItemID[counter];
+    cout << "Enter price of your item: ";
+    cin >> ItemPrice[counter];
+    counter++;
+}
+
+void Shop ::displayPrice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "The price of item with ID is " << ItemID[i] << " is " << ItemPrice[i] << endl;
+    }
+}
+
+int main()
+{
+    // object declaration
+    Shop obj;
+
+    // function call
+    obj.initCounter();
+    obj.setPrice();
+    obj.setPrice();
+    obj.setPrice();
+    obj.displayPrice();
+
+    return 0;
+}
