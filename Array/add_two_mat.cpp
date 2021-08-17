070e38caed77b0d2e709d1c5a71cdba566ed0090
Vanshika <vanshikag0111@gmail.com>
2021-08-17 12:58:39 +0530

    add two sqaure matrices

---
+++ Array/add_two_mat.cpp
@@ -0,0 +1,90 @@
+// program to add two matrices
+
+#include<iostream>
+#include<stdlib.h>
+#include<process.h>
+using namespace std;
+
+int main()
+{
+    int a[10][10], b[10][10], c[10][10];
+    int i,j,m,n,p,q;
+
+    cout << "Enter number of rows for matrix A: ";
+    cin >> m;
+    cout << "Enter number of columns for matrix A: ";
+    cin >> n;
+    cout << "Enter number of rows for matrix B: ";
+    cin >> p;
+    cout << "Enter number of columns for matrix B: ";
+    cin >> q;
+    cout << endl;
+
+    if((m==p) && (n==q))
+    {
+        cout << "Matrices can be added" << endl;
+    }
+    else
+    {
+        cout << "Matrices cannot be added." << endl;
+        exit(0);
+    }
+
+    cout << "INPUT MATRIX A" << endl;
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "INPUT MATRIX B" << endl;
+    for(i=0; i<p; i++)
+    {
+        for(j=0; j<q; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+
+    cout << "DISPLAYING MATRIX A" << endl;
+    for(i=0; i<m; i++)
+    {
+        cout << endl;
+        for(j=0; j<n; j++)
+        {
+            cout << " " << a[i][j];
+        }
+    }
+
+    cout << "DISPLAYING MATRIX B" << endl;
+    for(i=0; i<p; i++)
+    {
+        cout << endl;
+        for(j=0; j<q; j++)
+        {
+            cout << " " << b[i][j];
+        }
+    }
+
+    cout << "PROCESSING ADDITION OF MATRICES..." << endl;
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    cout << "SUM OF TWO GIVEN MATRICES IS " << endl;
+    for(i=0; i<m; i++)
+    {
+        cout << endl;
+        for(j=0; j<n; j++)
+        {
+            cout << " " << c[i][j];
+        }
+    }
+
+    return 0;
+}
---
+++ Array/max.cpp
@@ -0,0 +1,49 @@
+// program to find the largest element of an array
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    float large(float arr[], int n);
+
+    char ch;
+    int i;
+    float amount[50], big;
+
+    for(i=0; i<50; i++)
+    {
+        cout << "Enter element number " << i+1 << ": ";
+        cin >> amount[i];
+        cout << endl << "Want to enter more? (y/n): " ;
+        cin >> ch;
+        if(ch != 'y')
+        {
+            break;
+        }
+    }
+
+    if (i< 50)
+    {
+        i++;
+    }
+
+    big = large(amount, i);
+    cout << "The largest element of the array is " << big << endl;
+
+    return 0;
+}
+
+float large(float arr[], int n)
+{
+    float max = arr[0];
+    for (int j=1; j<n; j++)
+    {
+        if (arr[j] > max)
+        {
+            max = arr[j];
+        }
+    }
+    return (max);
+}
