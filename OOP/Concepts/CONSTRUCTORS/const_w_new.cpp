d50aa9436b1ee573c0177c43f187671960ab1096
Vanshika <vanshikag0111@gmail.com>
2021-08-24 10:47:24 +0530

    constructor with new operator

---
+++ OOP/Concepts/CONSTRUCTORS/const_w_new.cpp
@@ -0,0 +1,57 @@
+// constructor with new operator
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class String
+{
+    char *name;
+    int length;
+
+    public:
+        String()
+        {
+            length = 0;
+            name = new char[length+1];
+        }
+
+        String(char *s)
+        {
+            length = strlen(s);
+            name = new char[length+1];
+            strcpy(name,s);
+        }
+
+        void display(void)
+        {
+            cout << name << endl;
+        }
+
+        void join(String &a, String &b);
+};
+
+void String :: join(String &a, String &b)
+{
+    length = a.length + b.length;
+    delete name;
+    name = new char[length+1];
+    strcpy(name, a.name);
+    strcat(name, b.name);
+}
+
+int main()
+{
+    char *first = "john";
+    String name1(first), name2("low"), name3("high"), s1,s2'
+
+    s1.join(name1,name2);
+    s2.join(s1,name3);
+    name1.display();
+    name2.display();
+    name3.display();
+    s1.display();
+    s2.display();
+    
+    return 0;
+}
